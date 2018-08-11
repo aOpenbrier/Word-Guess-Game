01c9a8df02966809f8959461bc8f4e910aee05fe
aOpenbrier <aopenbrier@gmail.com>
2018-08-11 10:29:35 -0700

    psuedocode layout

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+// create game object
+    //let gameState = {
+        //isGameStarted: false,
+        //wins: 0,
+        //guessesNum: 8,
+        //wordsArray: ['example', 'another', 'lastly'],
+        //currentWordindex: 0,
+        //currentWord: 
+        //currentWordArr: currentWord.split()
+        //}
+
+//New game function
+    //get current word from list of words
+    //split current word into array
+    //display current word as underscores
+    //reset guesses
+
+//on key event:
+    //if game is started:
+        //check if letter is in word
+            //true:change underscore to letter
+                //check if game is complete
+                    //true: congrats, start new game
+                    //add 1 to win count, add 1 to word index
+            //false: deduct from number of guesses
+                //if at zero, sorry, start new game, add 1 to word index
+        // display letter as already guessed 
+    //if game isn't started,
+        //start game
+        //run new game function
+
