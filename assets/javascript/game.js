a659b83300d86baafbd9cf6fdc07f7fbb5e164a8
aOpenbrier <aopenbrier@gmail.com>
2018-08-16 21:29:57 -0700

    decrease difficulty

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,12 +2,18 @@
 let gameState = {
     isGameStarted: false,
     winCount: 0,
-    guessCount: 6,
-    wordsArr: [`example`, `another`, `lastly`], //use 2 spaces for multiple words
+    guessCount: 10,
+    wordsArr: [`CIRCUS`, `STRONGER`, `MAKE ME OOH`, `EVERYTIME`, `IF YOU SEEK AMY`, `FEMME FATALE`, `GIMME MORE`, `WOMANIZER`, `TOXIC`, `SLUMBER PARTY`, `OOPS I DID IT AGAIN`, `BLACKOUT`, `MY PREROGATIVE`, `BREAK THE ICE`, `LUCKY`, `OVER PROTECTED`], //use 2 spaces for multiple words
     activeWordIndex: 0,
-    activeWordArr: [``],
-    hiddenWordArr: [``],
-    lettersGuessed: [``]
+    activeWordArr: [],
+    hiddenWordArr: [],
+    lettersGuessed: [],
+    updateValues(){
+        document.querySelector('#hidden').innerHTML = this.hiddenWordArr.join(`&nbsp;`)
+        document.querySelector('#guesscount').innerHTML = this.guessCount
+        document.querySelector('#letters').innerHTML = this.lettersGuessed.join('&nbsp;')
+        document.querySelector('#wincount').innerHTML = this.winCount
+    }
 }
 
 function newWord() {
@@ -20,53 +26,46 @@ function newWord() {
                 gameState.hiddenWordArr[i] = '_'
             }
             else{
-                gameState.hiddenWordArr[i] = gameState.activeWordArr[i]
+                gameState.hiddenWordArr[i] = `&nbsp;`
             }
         }
-        document.querySelector('#hidden').innerHTML = gameState.hiddenWordArr.join(`&nbsp;`) //display hidden game word
-        gameState.guessCount = 6
-        document.querySelector('#guesscount').innerHTML = gameState.guessCount //reset guess count
+        gameState.guessCount = 10
         gameState.lettersGuessed = ['&nbsp;']
-        document.querySelector('#letters').innerHTML = gameState.lettersGuessed //reset letters guessed
         gameState.activeWordIndex++ //ready for next round
     }
     else {
         document.querySelector('#message').innerHTML = `That's all for now. Press any key to start over`
         gameState.isGameStarted = false
         gameState.winCount = 0
+        gameState.activeWordIndex = 0
+
     }
+    gameState.updateValues()
 }
 
 //on key event:
 document.onkeyup = function (event) {
-    console.log(`onkeyup triggered`) //TESTING
     if (gameState.isGameStarted) { //if game was started
         if (/[a-z]/i.test(event.key)) { //if key pressed was a letter
-            console.log(`tested if it's a letter`)
-            if (gameState.lettersGuessed.indexOf(event.key) === -1) { //if it's not already guessed
-                gameState.lettersGuessed.push(event.key)
-                document.querySelector('#letters').innerHTML = gameState.lettersGuessed.join('  ') //update letters guessed
-                if (gameState.activeWordArr.indexOf(event.key) > -1) { 
-                    gameState.activeWordArr.forEach(function (letter, i) {
-                        if (letter === event.key) {
+            let keyPressed=event.key.toUpperCase()
+            console.log(keyPressed)
+            if (gameState.lettersGuessed.indexOf(keyPressed) === -1) { //if it hasn't been guessed yet
+                gameState.lettersGuessed.push(keyPressed)
+                if (gameState.activeWordArr.indexOf(keyPressed) > -1) { //if letter is in the array at all
+                    gameState.activeWordArr.forEach(function (letter, i) { //replace any underscores with letter
+                        if (letter === keyPressed) {
                             gameState.hiddenWordArr[i] = gameState.activeWordArr[i]
                         }
                     })
-                    document.querySelector('#hidden').innerHTML = gameState.hiddenWordArr.join(`&nbsp;`) //display hidden game word
-                    console.log(`active '${gameState.activeWordArr}'`)
-                    console.log(`hidden '${gameState.hiddenWordArr}'`)
-                    console.log(`index of underscore: ${gameState.hiddenWordArr.indexOf('_')}`)
-                    if (gameState.hiddenWordArr.indexOf('_') === -1) {
-                        document.querySelector('#message').innerHTML = 'You won!'
+                    if (gameState.hiddenWordArr.indexOf('_') === -1) { //if there aren't any underscores remaining
+                        document.querySelector('#message').innerHTML = `You won! '${gameState.activeWordArr.join('')}' is correct`
                         gameState.winCount++
-                        document.querySelector('#wincount').innerHTML = gameState.winCount
                         newWord()
                     }
                 }
-                else {
+                else { //letter wasn't found in word
                     gameState.guessCount--
-                    document.querySelector('#guesscount').innerHTML = gameState.guessCount
-                    if (gameState.guessCount < 1) {
+                    if (gameState.guessCount < 1) { //if there are no guesses remaining
                         document.querySelector('#message').innerHTML = `Sorry, you lost. Try the next word`
                         newWord()
                     }
@@ -77,10 +76,10 @@ document.onkeyup = function (event) {
             }
         }
     }
-    else { //game isn't started
+    else { //game hasn't started
         gameState.isGameStarted = true
-        console.log(`game is now started`)
         document.querySelector('#message').innerHTML = `Type in your first guess`
         newWord()
     }
+    gameState.updateValues()
 }
